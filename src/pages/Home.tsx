6e2f45d56b2c432067129cd34ab01a18c7f05ffc
Aloksh42 <alokbits02@gmail.com>
2024-02-24 20:14:56 +0530

    Added table & rows

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,64 @@
+import { useEffect, useState } from "react";
 
-const Home:React.FC = () => {
-  return (
-    <div>HomePage</div>
-  )
+interface Problem {
+    id: number;
+    problemStatement: string;
+    problemLink: string;
+    solutionLink: string;
+    rating: number;
 }
 
-export default Home
+const Home: React.FC = () => {
+    const [problems, setProblems] = useState<Problem[]>([]);
+
+    useEffect(() => {
+        fetch("/db.json")
+            .then((res) => res.json())
+            .then((data) => setProblems(data))
+            .catch((error) => console.error("Error fetching problems:", error));
+    }, []);
+
+    return (
+        <div className="container">
+            <p>
+                <strong>Tip:</strong>Going to solution without attempting the
+                question is waste of your time{" "}
+            </p>
+            <br />
+            <br />
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>S.No.</th>
+                        <th>Problems</th>
+                        <th>Solution</th>
+                        <th>Rating</th>
+                        <th>Solved</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {problems.map((problem, index) => (
+                        <tr key={problem.id}>
+                            <td>{index + 1}.</td>
+                            <td>{problem.problemStatement}</td>
+                            <td>
+                                <a
+                                    href={problem.solutionLink}
+                                    style={{ textDecoration: "none" }}
+                                >
+                                    Solution
+                                </a>
+                            </td>
+                            <td>{problem.rating}</td>
+                            <td>
+                                <input type="checkbox" />
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Home;
